5079cb5d9d6ea9dbc9c0d20ad70778b20c1511ec
Zoxxs <136922170+Zoxxs@users.noreply.github.com>
2023-08-21 19:26:50 +0200

    add function for computer roll random generation

---
+++ rps.js
@@ -0,0 +1,23 @@
+// Rock Paper Scissors
+//
+// Player roll vs Computer roll
+//  play 5 rounds, store score until those 5 rounds were played
+//  display result 
+//
+// Player roll
+//   prompt for choice
+//   input should be case insensitive 
+//
+// Computer roll
+//   random generation
+
+
+
+// Computer roll 
+
+const getRandomChoice = (strings) => {
+  const choice = Math.floor(Math.random() * strings.length);
+  return strings[choice];
+};
+
+const strings = ["Rock", "Paper", "Scissors"];
