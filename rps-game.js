990f28e7142ba32d9a01800ed345d5f4690d1275
Zoxxs <136922170+Zoxxs@users.noreply.github.com>
2023-09-13 22:04:37 +0200

    Refactor game logic code with buttons instead of autoplay

--- rps-game.js
+++ rps-game.js
@@ -1,31 +1,23 @@
+// Game Logic
+
 const allChoices = ["rock", "paper", "scissors"];
+let choice = ``;
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * allChoices.length);
   return allChoices[randomNumber];
 }
 
-function getPlayerChoice() {
-  let playerInput;
-  do {
-    playerInput = prompt("Please choose between rock, paper and scissors").toLowerCase();
-  } while (!allChoices.includes(playerInput));
-  return playerInput;
-}
-
-let computerSelection;
-let playerSelection;
-
-let playerScore = 0;
-let computerScore = 0;
-let roundWinner = "";
-
 function playRound(playerSelection, computerSelection) {
+  playerSelection = choice;
+  console.log(playerSelection);  
   computerSelection = getComputerChoice(allChoices);
   console.log(computerSelection);
-  playerSelection = getPlayerChoice();
-  console.log(playerSelection);
-  
+    
   if (playerSelection == computerSelection) {
     console.log("Round is tied!");
     return;
@@ -37,7 +29,7 @@ function playRound(playerSelection, computerSelection) {
   ) {
     console.log("Player wins!");
     roundWinner = "player";
-    return playerScore += 1;
+    playerScore += 1;
   }
   if (
     (playerSelection == "rock" && computerSelection == "paper") ||
@@ -46,16 +38,45 @@ function playRound(playerSelection, computerSelection) {
   ) {
     console.log("Computer wins!");
     roundWinner = "computer";
-    return computerScore += 1;
+    computerScore += 1;
+  }
+  isGameOver(playerScore, computerScore);
+}
+
+function isGameOver(playerScore, computerScore) {
+  if (playerScore === 5) {
+    console.log(`Player wins the game!`);
+    resetGame();
+  }
+  if (computerScore === 5) {
+    console.log(`Computer wins the game!`);
+    resetGame();
   }
 }
 
-isGameOver();
-
-function isGameOver() {
-  do {
-    playRound(playerSelection, computerSelection);
-  } while (playerScore < 2 && computerScore < 2);  
+function resetGame() {
+  playerScore = 0; 
+  computerScore = 0;
 }
 
+// Game UI
+
+const rockPlayerChoice = document.querySelector(`#rock`);
+const paperPlayerChoice = document.querySelector(`#paper`);
+const scissorsPlayerChoice = document.querySelector(`#scissors`);
+
+rockPlayerChoice.addEventListener(`click`, () => {
+  choice = `rock`;
+  playRound();
+  console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+});
+paperPlayerChoice.addEventListener(`click`, () => {
+  choice = `paper`;
+  playRound();
 console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+});
+scissorsPlayerChoice.addEventListener(`click`, () => {
+  choice = `scissors`;
+  playRound();
+  console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+});
